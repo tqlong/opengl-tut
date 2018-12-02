6e00b231309c7260890b0185abab1ba7d140333e
Long <tqlong@gmail.com>
2018-12-03 00:56:43 +0700

    varying_color

--- varying_color.py
+++ varying_color.py
@@ -9,29 +9,32 @@ BaseContext = testingcontext.getInteractive()
 
 class TestContext (BaseContext):
     def OnInit(self):
-        VERTEX_SHADER = shaders.compileShader("""#version 120
+        VERTEX_SHADER = shaders.compileShader("""
+        varying vec4 vertex_color;
         void main() {
             gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            vertex_color = gl_Color;
         }""", GL_VERTEX_SHADER)
 
-        FRAGMENT_SHADER = shaders.compileShader("""#version 120
+        FRAGMENT_SHADER = shaders.compileShader("""
+        varying vec4 vertex_color;
         void main() {
-            gl_FragColor = vec4(0, 1, 0, 1);
+            gl_FragColor = vertex_color;
         }""", GL_FRAGMENT_SHADER)
 
         self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
 
         self.vbo = vbo.VBO(
             array([
-                [0, 1, 0],
-                [-1, -1, 0],
-                [1, -1, 0],
-                [2, -1, 0],
-                [4, -1, 0],
-                [4, 1, 0],
-                [2, -1, 0],
-                [4, 1, 0],
-                [2, 1, 0],
+                [0, 1, 0,  0, 1, 0],
+                [-1, -1, 0,  1, 1, 0],
+                [1, -1, 0,  0, 1, 1],
+                [2, -1, 0,  1, 0, 0],
+                [4, -1, 0,  0, 1, 0],
+                [4, 1, 0,  0, 0, 1],
+                [2, -1, 0,  1, 0, 0],
+                [4, 1, 0,  0, 0, 1],
+                [2, 1, 0,  0, 1, 1],
             ], 'f')
         )
 
@@ -42,11 +45,14 @@ class TestContext (BaseContext):
             self.vbo.bind()
             try:
                 glEnableClientState(GL_VERTEX_ARRAY)
-                glVertexPointerf(self.vbo)
+                glEnableClientState(GL_COLOR_ARRAY)
+                glVertexPointer(3, GL_FLOAT, 24, self.vbo)
+                glColorPointer(3, GL_FLOAT, 24, self.vbo+12)
                 glDrawArrays(GL_TRIANGLES, 0, 9)
             finally:
                 self.vbo.unbind()
                 glDisableClientState(GL_VERTEX_ARRAY)
+                glDisableClientState(GL_COLOR_ARRAY)
         finally:
             shaders.glUseProgram(0)
 
